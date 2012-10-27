603dbee38adb0d39d672f8b1e99df467e352f98f
Jiri Pospisil <mekishizufu@gmail.com>
2012-10-27 21:05:52 +0200

    First dummy version

--- lib/rock_config.rb
+++ lib/rock_config.rb
@@ -1,5 +1,38 @@
 require "rock_config/version"
+require "rock_config/environment_detector"
+require "rock_config/configuration"
+require "rock_config/manager"
 
 module RockConfig
-  # Your code goes here...
+  class ConfigNotFound < Exception; end
+
+  class << self
+    def for(config_name, environment = detect_environment)
+      manager.fetch(config_name, environment) do
+        raise ConfigNotFound, "Config not found"
+      end
+    end
+
+    def configure
+      yield configuration if block_given?
+      @manager = nil
+    end
+
+    private
+
+    def detect_environment
+      @detected_environment ||= lambda do
+        detector = EnvironmentDetector.new(ENV)
+        detector.detect
+      end.call
+    end
+
+    def configuration
+      @configuration ||= Configuration.new
+    end
+
+    def manager
+      @manager ||= Manager.new(configuration)
+    end
+  end
 end
