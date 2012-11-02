c219e3c6bc16b4cf9d0be060c1f5f5e38b9926a9
Jiri Pospisil <mekishizufu@gmail.com>
2012-11-02 14:11:08 +0100

    Support for config querying

--- lib/rock_config/config.rb
+++ lib/rock_config/config.rb
@@ -3,5 +3,19 @@ module RockConfig
     def initialize(hash)
       @hash = hash
     end
+
+    def method_missing(*args, &block)
+      if value = @hash[args.first.to_s]
+        if Hash === value
+          Config.new(value)
+        else
+          value
+        end
+      end
+    end
+
+    def raw
+      @hash.dup
+    end
   end
 end
---
+++ spec/config_spec.rb
@@ -0,0 +1,54 @@
+require "spec_helper"
+
+module RockConfig
+  describe Config do
+    it "accepts a hash" do
+      hash = {foo: "bar"}
+
+      config = Config.new(hash)
+      config.raw.should eq(hash)
+    end
+
+    it "returns config for given environment" do
+      subhash_dev  = {"a" => 123}
+      subhash_prod = {"a" => 321}
+      hash = {
+        "development" => subhash_dev,
+        "production"  => subhash_prod
+      }
+
+      config = Config.new(hash)
+      config.development.raw.should eq(subhash_dev)
+      config.production.raw.should eq(subhash_prod)
+    end
+
+    it "returns nil when asked for non existing environment" do
+      config = Config.new({})
+      config.for_environment("I do not exist").should be_nil
+    end
+
+    it "returns correct values when asked for" do
+      hash = {
+        "development" => {
+          "host" => "localhost"
+        }
+      }
+
+      config = Config.new(hash)
+      config.development.host.should eq("localhost")
+    end
+
+    it "returns correct nested values when asked for" do
+      hash = {
+        "development" => {
+          "elastic" => {
+            "host" => "localhost"
+          }
+        }
+      }
+
+      config = Config.new(hash)
+      config.development.elastic.host.should eq("localhost")
+    end
+  end
+end
